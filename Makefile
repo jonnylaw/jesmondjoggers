c798282f8f004476640895000a7e7ba6eed5cb1d
Jonny Law <law.jonny@googlemail.com>
2020-01-17 08:38:38 +0000

    Added action and cleaned up into package

--- Makefile
+++
@@ -1,2 +0,0 @@
-site:
-	R -e "rmarkdown::render_site()"
--- scripts/read_results.R
+++ scripts/read_results.R
@@ -1,15 +1,4 @@
-read_results <- function(filename) {
-  year_handicap <- stringr::str_extract(string = filename, pattern = "[0-9]+")
-  read_csv(file = filename, col_types = "iccccccc") %>%
-    mutate(handicap = year_handicap)
-}
+library(jjshandicap)
 
-results_files <- fs::dir_ls(path = "results", pattern = "*.csv")
-
-all_results <- results_files %>%
-  map_dfr(read_results) %>%
-  mutate(
-    running_time = period_to_seconds(lubridate::ms(as.character(running_time))),
-    gross_time = period_to_seconds(lubridate::ms(as.character(gross_time)))
-  ) %>%
-  mutate(handicap_time = gross_time - running_time)
+all_results <- read_all_results()
+readr::write_rds(all_results, "data/all_results.Rds")
